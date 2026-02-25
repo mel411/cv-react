918f360e5c4d22c606166b482d55092c27e17612
mel411 <melinayorgovaa@gmail.com>
2026-02-26 00:27:11 +0100

    Fix vite base for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/cv-react/',
+  base: "/cv-react/",
   plugins: [react()],
 })
